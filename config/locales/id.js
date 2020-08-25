5a50c7e29e1cfd1024f58ea25c87f4d80d898e85
ulyadev <70010338+ulyadev@users.noreply.github.com>
2020-08-25 18:57:49 +0700

    Update id.js

--- config/locales/id.js
+++ config/locales/id.js
@@ -1,13 +1,13 @@
 module.exports = {
   tHome: 'Beranda',
-  tRelativePosts: 'Publikasi',
+  tRelativePosts: 'Artikel Lainnya',
   tFollowTwitterDescription: 'Ikuti Saya di Twitter',
   tTags: 'Tagar',
-  tIndTitle: 'Semua Publikasi',
-  taIndKeywords: [`blog`, `publikasi`, `pribadi`, `dokumentasi`],
-  tfIndCountPosts: count => `${count} Publikasi`,
+  tIndTitle: 'Semua Artikel',
+  taIndKeywords: [`blog`, `publikasi`, `pribadi`, `dokumentasi`, `catatan`, `pengalaman`],
+  tfIndCountPosts: count => `${count} Artikel`,
   tfTagHeader: (totalCount, tag) =>
     `${totalCount} post${totalCount === 1 ? '' : 's'} tagged with "${tag}"`,
-  t404Title: 'Halaman Tidak Ada',
-  t404Content: 'Ups! mungkin anda salah mengakses URL.',
+  t404Title: 'Halaman tidak tersedia',
+  t404Content: 'Ups! URL yang anda akses tidak ditemukan.',
 };
